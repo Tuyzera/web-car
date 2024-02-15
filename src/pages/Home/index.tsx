707a9eade80258c86621048b0a383dad8146ee55
Tuyzera <utpadelarthur@gmail.com>
2024-02-15 20:29:12 -0300

    ..

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
+import { Container } from "../../components/Container";
 
 export function Home() {
 
   return (
-    <div>
-     Home
-    </div>
+    <Container>
+         <section className="w-full flex justify-center p-3 rounded-2xl bg-white justify-around gap-5">
+            <input placeholder="Digite o nome do carro" className="w-full rounded-xl border-2 p-2 text-black"/>
+            <button className="bg-red-600 px-20 rounded-2xl text-white">Buscar</button>
+         </section>
+    </Container>
+   
   )
 }
 
